03c488a1f73e7f518574f722f5e4c5362d181480
Mr.Shital B.Madake <sbmadake@gmail.com>
2021-08-25 09:40:33 +0530

    Initial commit

---
+++ BankingApplication/src/com/training/org/MainClass.java
@@ -0,0 +1,14 @@
+package com.training.org;
+
+public class MainClass {
+    public static void main(String[] args) {
+        XYZBank x1=new XYZBank();
+        System.out.println(x1);
+
+
+        XYZBank x2=new XYZBank("MYBank","USA",40000);
+        System.out.println(x2);
+
+
+    }
+}
